5db6fa0e3a5007cd5efb0f8dec9adc3ceb75e568
milo <mroberts18@bristolcc.edu>
2024-01-27 20:43:59 -0500

    Initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="./app.css">
+
+<body>
+    <article>
+        <h1>My First Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui corporis odio tempore earum, enim quibusdam a quis rem ullam? Temporibus, sit aliquid iure rerum explicabo rem alias ut! Ad delectus id aut tempore blanditiis? Sunt, repudiandae impedit. Neque veritatis commodi eligendi fugiat ducimus odit ipsa necessitatibus? Sequi, repudiandae. Ipsam, aliquam.</p>
+    </article>
+    <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="./app.css">
+
+<body>
+    <article>
+        <h1>
+            <a href="/post">My First Post</a>
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui corporis odio tempore earum, enim quibusdam a quis rem ullam? Temporibus, sit aliquid iure rerum explicabo rem alias ut! Ad delectus id aut tempore blanditiis? Sunt, repudiandae impedit. Neque veritatis commodi eligendi fugiat ducimus odit ipsa necessitatibus? Sequi, repudiandae. Ipsam, aliquam.</p>
+    </article>
+
+    <article>
+        <h1>
+            <a href="/post">My Second Post</a>
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui corporis odio tempore earum, enim quibusdam a quis rem ullam? Temporibus, sit aliquid iure rerum explicabo rem alias ut! Ad delectus id aut tempore blanditiis? Sunt, repudiandae impedit. Neque veritatis commodi eligendi fugiat ducimus odit ipsa necessitatibus? Sequi, repudiandae. Ipsam, aliquam.</p>
+    </article>
+
+    <article>
+        <h1>
+            <a href="/post">My Third Post</a>
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui corporis odio tempore earum, enim quibusdam a quis rem ullam? Temporibus, sit aliquid iure rerum explicabo rem alias ut! Ad delectus id aut tempore blanditiis? Sunt, repudiandae impedit. Neque veritatis commodi eligendi fugiat ducimus odit ipsa necessitatibus? Sequi, repudiandae. Ipsam, aliquam.</p>
+    </article>
+</body>
