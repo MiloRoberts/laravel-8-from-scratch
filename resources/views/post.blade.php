9132e60632e403b2f47e031678adc3f2ac25382b
milo <mroberts18@bristolcc.edu>
2024-01-31 11:20:05 -0500

    Completed through episode 11

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,8 +5,7 @@
 
 <body>
     <article>
-        <h1>My First Post</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui corporis odio tempore earum, enim quibusdam a quis rem ullam? Temporibus, sit aliquid iure rerum explicabo rem alias ut! Ad delectus id aut tempore blanditiis? Sunt, repudiandae impedit. Neque veritatis commodi eligendi fugiat ducimus odit ipsa necessitatibus? Sequi, repudiandae. Ipsam, aliquam.</p>
+        <?= $post ?>
     </article>
     <a href="/">Go Back</a>
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,24 +4,29 @@
 <link rel="stylesheet" href="./app.css">
 
 <body>
-    <article>
+    <?php foreach($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
+    <!-- <article>
         <h1>
-            <a href="/post">My First Post</a>
+            <a href="/posts/my-first-post">My First Post</a>
         </h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui corporis odio tempore earum, enim quibusdam a quis rem ullam? Temporibus, sit aliquid iure rerum explicabo rem alias ut! Ad delectus id aut tempore blanditiis? Sunt, repudiandae impedit. Neque veritatis commodi eligendi fugiat ducimus odit ipsa necessitatibus? Sequi, repudiandae. Ipsam, aliquam.</p>
     </article>
 
     <article>
         <h1>
-            <a href="/post">My Second Post</a>
+            <a href="/posts/my-second-post">My Second Post</a>
         </h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui corporis odio tempore earum, enim quibusdam a quis rem ullam? Temporibus, sit aliquid iure rerum explicabo rem alias ut! Ad delectus id aut tempore blanditiis? Sunt, repudiandae impedit. Neque veritatis commodi eligendi fugiat ducimus odit ipsa necessitatibus? Sequi, repudiandae. Ipsam, aliquam.</p>
     </article>
 
     <article>
         <h1>
-            <a href="/post">My Third Post</a>
+            <a href="/posts/my-third-post">My Third Post</a>
         </h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui corporis odio tempore earum, enim quibusdam a quis rem ullam? Temporibus, sit aliquid iure rerum explicabo rem alias ut! Ad delectus id aut tempore blanditiis? Sunt, repudiandae impedit. Neque veritatis commodi eligendi fugiat ducimus odit ipsa necessitatibus? Sequi, repudiandae. Ipsam, aliquam.</p>
-    </article>
+    </article> -->
 </body>
