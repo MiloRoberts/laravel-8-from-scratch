7330b568419ae9105df1bd066d880bcb2df77c1d
milo <mroberts18@bristolcc.edu>
2024-02-07 09:36:29 -0500

    Completed through lesson 18

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,39 +23,59 @@ class Post {
 
     public static function all(){
         return cache()->rememberForever('posts.all', function() {
-            $files = File::files(resource_path("posts/"));
-    
-            $posts = array_map(function($file) {
-                $document = YamlFrontMatter::parseFile($file);
-                return new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
-                );
-            }, $files);
+            return collect(File::files(resource_path("posts")))->map(fn($file) => YamlFrontMatter::parseFile($file))->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ))->sortByDesc('date');
 
-            usort($posts, function ($x, $y) {
-                if ($x->date == $y->date) {
-                    return 0;
-                } else if (($x->date > $y->date)) {
-                    return -1;
-                } else {
-                    return 1;
-                }
-            });
+            // $files = File::files(resource_path("posts/"));
+    
+            // $posts = array_map(function($file) {
+            //     $document = YamlFrontMatter::parseFile($file);
+            //     return new Post(
+            //         $document->title,
+            //         $document->excerpt,
+            //         $document->date,
+            //         $document->body(),
+            //         $document->slug
+            //     );
+            // }, $files);
+
+            // usort($posts, function ($x, $y) {
+            //     if ($x->date == $y->date) {
+            //         return 0;
+            //     } else if (($x->date > $y->date)) {
+            //         return -1;
+            //     } else {
+            //         return 1;
+            //     }
+            // });
             
-            return $posts;
+            // return $posts;
         });
     }
         
     public static function find($slug) {
-        $posts = static::all();
-        $filteredPosts = array_filter($posts, function($post) use ($slug){
-            return $post->slug == $slug;
-        });
+        return static::all()->firstWhere('slug', $slug);
+
+        // $posts = static::all();
+        // $filteredPosts = array_filter($posts, function($post) use ($slug){
+        //     return $post->slug == $slug;
+        // });
         
-        return (array_values($filteredPosts))[0];
+        // return (array_values($filteredPosts))[0];
+    }
+
+    public static function findOrFail($slug) {
+        $post = static::find($slug);
+
+        if (! $post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
     }
 }
