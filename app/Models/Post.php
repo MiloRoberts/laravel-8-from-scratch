a7e3d8aa34729c7b90e830f5dbf2e287843d8b99
milo <mroberts18@bristolcc.edu>
2024-02-12 15:29:58 -0500

    Completed through lesson 22

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,80 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post {
-    public $title;
-    public $excerpt;
-    public $date;
-    public $body;
-    public $slug;
+class Post extends Model
+{
+    use HasFactory;
 
-    public function __construct($title, $excerpt, $date, $body, $slug){
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function all(){
-        return cache()->rememberForever('posts.all', function() {
-            return collect(File::files(resource_path("posts")))->map(fn($file) => YamlFrontMatter::parseFile($file))->map(fn($document) => new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            ))->sortByDesc('date');
-
-            // $files = File::files(resource_path("posts/"));
-    
-            // $posts = array_map(function($file) {
-            //     $document = YamlFrontMatter::parseFile($file);
-            //     return new Post(
-            //         $document->title,
-            //         $document->excerpt,
-            //         $document->date,
-            //         $document->body(),
-            //         $document->slug
-            //     );
-            // }, $files);
-
-            // usort($posts, function ($x, $y) {
-            //     if ($x->date == $y->date) {
-            //         return 0;
-            //     } else if (($x->date > $y->date)) {
-            //         return -1;
-            //     } else {
-            //         return 1;
-            //     }
-            // });
-            
-            // return $posts;
-        });
-    }
-        
-    public static function find($slug) {
-        return static::all()->firstWhere('slug', $slug);
-
-        // $posts = static::all();
-        // $filteredPosts = array_filter($posts, function($post) use ($slug){
-        //     return $post->slug == $slug;
-        // });
-        
-        // return (array_values($filteredPosts))[0];
-    }
-
-    public static function findOrFail($slug) {
-        $post = static::find($slug);
-
-        if (! $post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
+    // protected $guarded = ['id'];
+    protected $fillable = ['title', 'excerpt', 'body'];
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,9 +4,9 @@
     @foreach($posts as $post)
         <article>
             <h1>
-                <a href="/posts/{{ $post->slug }}">
+                <a href="/posts/{{ $post->id }}">
                     <!-- <?= $post->title; ?> -->
-                    {{ $post->title }}
+                    {!! $post->title !!}
                 </a>
             </h1>
             <div>
